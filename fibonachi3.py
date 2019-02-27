479206518162c4dc993326437d7abf872fbadc7e
fox9ra <fox9ra1@gmail.com>
2019-02-27 22:25:45 +0300

    FiboReshenitGood

--- fibonachi3.py
+++ fibonachi3.py
@@ -1,16 +1,36 @@
-from timeit import default_timer as timer
+#from timeit import default_timer as timer
 
-n, m = list(map(int, input().split()))
+n, m = map(int, input().split())
 
-def feb(k):
-	a, b = 0, 1
-	for i in range(2, k+1):
-	    a_tmp = a
-	    a = b
-	    b = a_tmp + b
-	return(b)
+'''
+c=0
+T = [0,1]
+for i in range(1,n):
+    #print (i)
+    #T.append((T[i-1]+T[i-2])%m)
+    T.append(T[i-1]+T[i-2])
+    c+=1
+    if (T[i]==1) and (T[i-1]==0):
+        break
 
-t = timer()
-print (feb(n)%m)
-elapsed = timer() - t
-print("{:.10f} secs.".format(elapsed))
+#t = timer()
+print(T[(n%c)],T)
+#elapsed = timer() - t
+#print("{:.10f} secs.".format(elapsed))
+'''
+fibPrev = 0
+fib = 1
+cached = [fibPrev, fib]
+
+for curr in range(1, n):
+    fibOld = fib
+    fib = (fib + fibPrev) % m
+    fibPrev = fibOld
+
+    if fibPrev == 0 and fib == 1:
+        cached.pop()
+        break
+    else:
+        cached.append(fib)
+
+print(cached[ n % len(cached)])
