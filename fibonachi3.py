840f6e80773545f2a1836d9824430db130917ccb
bagriy <bagriy@complete.ru>
2019-02-28 11:10:02 +0300

    Algoritms febonachi ne poluchaetsa

--- fibonachi3.py
+++ fibonachi3.py
@@ -1,10 +1,11 @@
 from timeit import default_timer as timer
-
+import functools
 n, m = list(map(int, input().split()))
 
+@functools.lru_cache(maxsize=None)
 def feb(k):
 	a, b = 0, 1
-	for i in range(2, k+1):
+	for i in range(1, k+1):
 	    a_tmp = a
 	    a = b
 	    b = a_tmp + b
